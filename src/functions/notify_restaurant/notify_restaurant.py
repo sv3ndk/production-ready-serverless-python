222137c3e368365f28aff3b2a8398a8649efc1e2
sv3ndk <account.github@svend.xyz>
2024-09-06 07:31:55 +0200

    now uses EventBridgeEvent event source

--- src/functions/notify_restaurant/notify_restaurant.py
+++ src/functions/notify_restaurant/notify_restaurant.py
@@ -2,6 +2,7 @@ import os
 import json
 import boto3
 from aws_lambda_powertools.logging import Logger
+from aws_lambda_powertools.utilities.data_classes import event_source, EventBridgeEvent
 
 logger = Logger(log_uncaught_exceptions=True)
 
@@ -16,10 +17,10 @@ if not bus_name:
 eventbridge_client = boto3.client("events")
 sns_client = boto3.client("sns")
 
+@event_source(data_class=EventBridgeEvent)
+def handler(event: EventBridgeEvent, context):
 
-def handler(event, context):
-
-    order = event["detail"]
+    order = event.detail
     sns_client.publish(
         TopicArn=topic_ARN,
         Message=json.dumps(order)
