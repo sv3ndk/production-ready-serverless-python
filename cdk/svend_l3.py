117286f4df28b8800274cbdcf0819afa2fa82cf3
sv3ndk <account.github@svend.xyz>
2024-09-27 09:22:32 +0200

    CDK L3 construct: now use 1G for all lambdas by default

--- cdk/svend_l3.py
+++ cdk/svend_l3.py
@@ -3,24 +3,6 @@ from aws_cdk import aws_lambda as lambda_
 from aws_cdk.aws_lambda_python_alpha import PythonFunction, PythonFunctionProps
 
 
-# class TracedPythonFunction(Construct):
-#     def __init__(self, scope: Construct, id: str, props: PythonFunctionProps) -> None:
-#         super().__init__(scope, id)
-#
-#         props2 = {
-#             k: v
-#             for k, v in props.__dict__["_values"].items()
-#         }
-#
-#         props2["tracing"] = lambda_.Tracing.ACTIVE
-#
-#         self.lambda_function = PythonFunction(self, "Traced", **props2)
-#
-#     def function(self) -> PythonFunction:
-#         return self.lambda_function
-
-# remove the class, make it a function that returns the PythonFunction
-
 def traced_python_function(scope: Construct, id: str, props: PythonFunctionProps) -> PythonFunction:
     props2 = {
         k: v
@@ -28,5 +10,8 @@ def traced_python_function(scope: Construct, id: str, props: PythonFunctionProps
     }
 
     props2["tracing"] = lambda_.Tracing.ACTIVE
+
+    if "memory_size" not in props2:
+        props2["memory_size"] = 1024
 
     return PythonFunction(scope, id, **props2)
